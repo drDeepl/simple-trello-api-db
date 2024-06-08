cda5b221d24a87d0e3739c1a2a6ef2310c373dfa
VleeplPi <vlimaster@yandex.ru>
2024-06-08 11:24:11 +0700

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const docBuilder = new DocumentBuilder()
+    .setDescription('RESTful API схожий с trello')
+    .addTag('SimpleTrelloApp')
+    .build();
+  const document = SwaggerModule.createDocument(app, docBuilder);
+  SwaggerModule.setup('api/v1', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
