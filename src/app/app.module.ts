3ee21a3a21a1d24c5df9c9dffe687e9f8e10eb34
VleeplPi <vlimaster@yandex.ru>
2024-06-08 22:34:16 +0700

    added custom css to swagger page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
 
 @Module({
   imports: [
@@ -12,6 +15,10 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
       envFilePath: [`.env`],
       cache: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../..', 'wwwroot'),
+      serveRoot: '/api/wwwroot',
     }),
     UserModule,
     PrismaModule,
---
+++ src/swagger/constants/swagger-ui.constants.ts
@@ -0,0 +1,37 @@
+import { SwaggerUIConstants } from './interfaces/swagger-ui-constants.interface';
+
+export const SWAGGER_UI_CONSTANTS: SwaggerUIConstants = {
+  TOPBAR: {
+    BACKGROUND_COLOR: '#222222',
+  },
+  AUTHORIZE: {
+    BACKGROUND_COLOR: '#222222',
+  },
+  HTTP_METHODS: {
+    GET: {
+      BACKGROUND_COLOR: '#E4E4E4',
+      BORDER_COLOR: '#DADFE1',
+      SUMMARY_COLOR: 'rgba(97, 175, 254, 0.7)',
+    },
+    POST: {
+      BACKGROUND_COLOR: '#E4E4E4',
+      BORDER_COLOR: '#DADFE1',
+      SUMMARY_COLOR: 'rgba(73, 204, 144, 0.7)',
+    },
+    DELETE: {
+      BACKGROUND_COLOR: '#E4E4E4',
+      BORDER_COLOR: '#DADFE1',
+      SUMMARY_COLOR: 'rgba(249, 62, 62, 0.7)',
+    },
+    PATCH: {
+      BACKGROUND_COLOR: '#E4E4E4',
+      BORDER_COLOR: '#DADFE1',
+      SUMMARY_COLOR: 'rgba(80, 227, 194, 0.7)',
+    },
+    PUT: {
+      BACKGROUND_COLOR: '#E4E4E4',
+      BORDER_COLOR: '#DADFE1',
+      SUMMARY_COLOR: 'rgba(252, 161, 48, 0.7)',
+    },
+  },
+};
